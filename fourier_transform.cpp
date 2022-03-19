5c463c11e8622de70d91228037943f9ab41af4b7
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2022-03-19 23:21:30 +0200

    finishing first prototype of fourier transform

--- fourier_transform.cpp
+++ fourier_transform.cpp
@@ -1,17 +1,18 @@
 #include "fourier_transform.h"
-#include <iostream>
-using namespace std;
-
-Mat convertToFourier(Mat I)
+Mat convertToFourier(Mat img)
 {
-    Mat fourier;
-    Mat padded = adjustSize(I);
-
+    Mat padded = adjustSize(img);
     // Add to the expanded another plane with zeros
     Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
     Mat complexI = constructComplexNumbers(planes);
     dft(complexI, complexI);
-    return fourier;
+    split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+    magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+    Mat magnitude = prepareMagnitude(planes);
+    array<Mat,4> quarters = makeQuarters(magnitude);
+    quarters = reArrangeQuarters(quarters);
+    normalize(magnitude, magnitude, 0, 1, NORM_MINMAX);
+    return magnitude;
 }
 Mat adjustSize(Mat I)
 {
@@ -31,31 +32,42 @@ Mat constructComplexNumbers(Mat planes[])
     merge(planes, 2, complexI);
     return complexI;
 }
-    split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-    magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+Mat prepareMagnitude(Mat planes[])
+{
     Mat magI = planes[0];
     magI += Scalar::all(1);                    // switch to logarithmic scale
     log(magI, magI);
     // crop the spectrum, if it has an odd number of rows or columns
     magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
-    // rearrange the quadrants of Fourier image  so that the origin is at the image center
+    return magI;
+}
+
+array<Mat,4> makeQuarters(Mat magI)
+{
     int cx = magI.cols/2;
     int cy = magI.rows/2;
     Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
     Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
     Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
     Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
-    Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
-    q0.copyTo(tmp);
-    q3.copyTo(q0);
-    tmp.copyTo(q3);
-    q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
-    q2.copyTo(q1);
-    tmp.copyTo(q2);
-    normalize(magI, magI, 0, 1, NORM_MINMAX); // Transform the matrix with float values into a
-                                            // viewable image form (float between values 0 and 1).
-    imshow("Input Image"       , I   );    // Show the result
-    imshow("spectrum magnitude", magI);
-    waitKey();
-    return EXIT_SUCCESS;
+    array<Mat,4> quarters = {q0,q1,q2,q3};
+    return quarters;
 }
+// rearrange the quadrants of Fourier image  so that the origin is at the image center
+array<Mat,4> reArrangeQuarters(array<Mat,4> quarters)
+{
+    Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
+    quarters[0].copyTo(tmp);
+    quarters[3].copyTo(quarters[0]);
+    tmp.copyTo(quarters[3]);
+    quarters[1].copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+    quarters[2].copyTo(quarters[1]);
+    tmp.copyTo(quarters[2]);
+    return quarters;
+}
+// Transform the matrix with float values into a
+// viewable image form (float between values 0 and 1).
+// Show the result
+
+
+
--- fourier_transform.h
+++ fourier_transform.h
@@ -4,9 +4,15 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
+#include <array>
+#include <iostream>
+using namespace std;
 using namespace cv;
 
 Mat convertToFourier(Mat I);
 Mat adjustSize(Mat I);
 Mat constructComplexNumbers(Mat planes[]);
+Mat prepareMagnitude(Mat planes[]);
+array<Mat,4> makeQuarters(Mat magI);
+array<Mat,4> reArrangeQuarters(array<Mat,4> quarters);
 #endif // FOURIER_TRANSFORM_H
